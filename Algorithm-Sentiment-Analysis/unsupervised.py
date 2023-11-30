a01a3b89a077562757c69ca29fb692b219dda1a6
William Sanefski <william2.sanefski@gmail.com>
2023-11-30 13:18:42 -0500

    trasnformed this code to be object oriented

--- Algorithm-Sentiment-Analysis/unsupervised.py
+++ Algorithm-Sentiment-Analysis/unsupervised.py
@@ -1,6 +1,6 @@
 from statsmodels.regression.rolling import RollingOLS
 import pandas_datareader.data as web
-import matplotlib.pyplot as pit
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 import pandas as pd
 import numpy as np
@@ -10,26 +10,35 @@ import pandas_ta
 import warnings 
 warnings.filterwarnings('ignore')
 
-data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
-data['Symbol'] = data['Symbol'].str.replace('.', '-')
+class FinancialDataDownloader:
+    def __init__(self):
+        self.data = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
+        self.data['Symbol'] = self.data['Symbol'].str.replace('.', '-')
+        self.data_symbols = self.data['Symbol'].unique().tolist()
+        self.failed_download = []
+        self.finance_data = pd.DataFrame()
 
-data_symbols = data['Symbol'].unique().tolist()
+    def download_data(self, start_date, end_date):
+        for symbol in self.data_symbols:
+            try:
+                historical_data = yf.download(tickers=symbol, start=start_date, end=end_date)
+                self.finance_data = pd.concat([self.finance_data, historical_data], axis=1)
+            except Exception as e:
+                print(f"Failed to download data for {symbol}: {e}")
+                self.failed_download.append(symbol)
 
-end_date = input("Enter the end date (YYYY-MM-DD): ")
-start_date = pd.to_datetime(end_date)-pd.DateOffset(years=8)
+    def display_results(self):
+        print("\nFailed downloads:", self.failed_download)
+        print("\nFinancial data")
+        print(self.finance_data.head())
 
-failed_download = []
-finance_data = pd.DataFrame()
-for symbol in data_symbols:
-    try:
-        historical_data = yf.download(tickers=symbol, start=start_date, end=end_date)
-        finance_data = pd.concat([finance_data, historical_data], axis=1)
-    except Exception as e:
-        print("Failed to download data for {symbol}: {e}")
-        failed_download.append(symbol)
-    
-print("\nFailed downloads:", failed_download)
-print("\nFinancial data")
-print(finance_data.head())
+    def main(self):
+        end_date = input("Enter the end date (YYYY-MM-DD): ")
+        start_date = pd.to_datetime(end_date) - pd.DateOffset(years=8)
+        self.download_data(start_date, end_date)
+        self.display_results()
 
-
+# Instantiate the class and call the main method
+if __name__ == "__main__":
+    financial_downloader = FinancialDataDownloader()
+    financial_downloader.main()
