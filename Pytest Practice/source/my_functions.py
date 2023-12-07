5fc3738072bb2513f9de3c91fc305d0c6df94790
William Sanefski <william2.sanefski@gmail.com>
2023-12-07 14:12:06 -0500

    this is cleaning up the functions to inhereit class based testing

--- Pytest Practice/source/my_functions.py
+++ Pytest Practice/source/my_functions.py
@@ -1,13 +1,18 @@
-def add(num_1, num_2):
-    return num_1 + num_2
+class Functions():
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
 
-def subtract(num_1, num_2):
-    return num_1 - num_2
+    def add(self):
+        return self.num_1 + self.num_2
 
-def multiply(num_1, num_2):
-     return num_1 * num_2
+    def subtract(self):
+        return self.num_1 - self.num_2
 
-def divide(num_1, num_2):
-    if num_2 == 0:
-        raise(ValueError)
-    return num_1/num_2
+    def multiply(self):
+         return self.num_1 * self.num_2
+
+    def divide(self):
+        if self.num_2 == 0:
+            raise(ValueError)
+        return self.num_1/self.num_2
