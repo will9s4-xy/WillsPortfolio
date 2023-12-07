c2bab430b613a552cd67f3c2fcc9c254da824f6b
William Sanefski <william2.sanefski@gmail.com>
2023-12-07 14:27:45 -0500

    Added more clases to my functions.py this will be important later

--- Pytest Practice/source/my_functions.py
+++ Pytest Practice/source/my_functions.py
@@ -1,17 +1,30 @@
-class Functions():
+class Add_Functions():
     def __init__(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
 
     def add(self):
         return self.num_1 + self.num_2
+class Subtract_function():
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
 
     def subtract(self):
         return self.num_1 - self.num_2
+class Multiply_function():
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
 
     def multiply(self):
          return self.num_1 * self.num_2
 
+class Divide_function():
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2    
+    
     def divide(self):
         if self.num_2 == 0:
             raise(ValueError)
