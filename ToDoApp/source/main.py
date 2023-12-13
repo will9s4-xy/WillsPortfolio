558194c32c14c5cca87c7c34b186d6376ac8176e
William Sanefski <william2.sanefski@gmail.com>
2023-12-13 16:38:19 -0500

    completed my todoApplication

--- ToDoApp/source/main.py
+++ ToDoApp/source/main.py
@@ -1,32 +1,25 @@
-import sqlite3
-import os
-def create_tasks_table():
-    # Connect to the database
-    conn = sqlite3.connect('todo.db')
-    
-    # Create a cursor
-    db_cursor = conn.cursor()
-    
-    # SQL statement to create the tasks table
-    create_table_sql = '''
-        CREATE TABLE IF NOT EXISTS tasks (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            task TEXT NOT NULL,
-            done BOOLEAN NOT NULL DEFAULT FALSE
-        )
-    '''
-    
-    # Execute the SQL statement
-    db_cursor.execute(create_table_sql)
-    
-    # Commit the changes
-    conn.commit()
-    
-    # Close the connection
-    conn.close()
+from flask import Flask, render_template, request, redirect, url_for
 
-# Call the method to create the tasks table
-if not os.path.exists('todo.db'):
-    create_tasks_table()
+app = Flask(__name__)
 
+tasks = []
 
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add_task', methods=['POST'])
+def add_task():
+    new_task = request.form.get('task')
+    if new_task:
+        tasks.append(new_task)
+    return redirect(url_for('index'))
+
+@app.route('/delete_task/<int:task_id>')
+def delete_task(task_id):
+    if 0 <= task_id < len(tasks):
+        del tasks[task_id]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
