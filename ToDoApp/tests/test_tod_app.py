7ccce86d14dec8eef3b42b26b77edea1fa22a968
William Sanefski <william2.sanefski@gmail.com>
2023-12-14 14:13:53 -0500

    getting test set up for tomorrow

---
+++ ToDoApp/tests/test_tod_app.py
@@ -0,0 +1,18 @@
+from source import main  
+import pytest 
+
+def test_add_task():
+    pass
+
+def test_delete_task():
+    pass
+
+def test_edit_task():
+    pass
+
+def test_save_tasks():
+    pass
+
+def test_load_tasks():
+    pass
+
